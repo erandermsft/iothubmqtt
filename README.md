0b0b3cc8024292791b05d959711130f5d25a415b
Erik Andersson <erander@microsoft.com>
2019-08-15 19:09:48 +0200

    Added Python samples.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # iothubmqtt
+Shows two ways of connecting to IOT Hub directly over MQTT.
+
+Make sure to install IOT Extensions for Azure CLI first:
+
+
+`az extension add --name azure-cli-iot-ext`
+## X509 Cert
+
+1) Create a device identity with a self-signed cert that is valid for 10 days using <br>
+   `az iot hub device-identity create -n <hubname> -d <deviceid> --am x509_thumbprint --valid-days 10 --od .`<br>
+   This will generate two files `<deviceid>-cert.pem` and `<deviceid>-key.pem` in the current folder
+2) Modify iothubcert.py by setting:<br>
+    `device_id=<deviceid>` used in step 1 <br>
+    `iot_hub_name=<hubname>` used in step 1 <br>
+3) Run iothubcert.py. It should connect and send one message to IOT Hub over MQTT.
+## SAS
+1) Create a device identity with a SAS Token:
+    `az iot hub generate-sas-token -d <deviceid> -n <hubname>`
+2) Modify iothubsas.py by setting:
+    `device_id=<deviceid>` used in step 1 <br>
+    `iot_hub_name=<hubname>` used in step 1 <br>
+    `sas_token=<value of sas token>` generated in step 1 <br> 
+3) Run iothubsas.py. It should connect and send one message to IOT Hub over MQTT.
---
+++ iothubsas.py
@@ -0,0 +1,42 @@
+from paho.mqtt import client as mqtt
+import ssl
+
+path_to_root_cert = "./digicertroot.cer"
+device_id = ""
+
+#Can be generated using AZ CLI: az iot hub generate-sas-token -d <devicename> -n <hubname>
+#az iot hub generate-sas-token -d gateway2 -n contoso-iot-hub
+sas_token = ""
+#The name of the IOT Hub, i.e contoso-iot-hub
+iot_hub_name = ""
+
+ 
+def on_connect(client, userdata, flags, rc):
+    print("Device connected with result code: " + str(rc))
+
+
+def on_disconnect(client, userdata, rc):
+    print("Device disconnected with result code: " + str(rc))
+
+
+def on_publish(client, userdata, mid):
+    print("Device sent message")
+
+
+client = mqtt.Client(client_id=device_id, protocol=mqtt.MQTTv311)
+
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_publish = on_publish
+
+client.username_pw_set(username=iot_hub_name+".azure-devices.net/" +
+                       device_id + "/?api-version=2018-06-30", password=sas_token)
+
+client.tls_set(ca_certs=path_to_root_cert, certfile=None, keyfile=None,
+               cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1, ciphers=None)
+client.tls_insecure_set(False)
+
+client.connect(iot_hub_name+".azure-devices.net", port=8883)
+
+client.publish("devices/" + device_id + "/messages/events/", "{id=123}", qos=1)
+client.loop_forever()
